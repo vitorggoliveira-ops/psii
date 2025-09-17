a9be1e0c89674c1eb9295ddf295c527a3df110e4
vitorggoliveira-ops <vitor_gg_oliveira@estudante.sesisenai.org.br>
2025-09-17 16:29:07 -0300

    commit5_html
    add html

--- dbescola/src/main/repository/DbescolaApplication.java
+++ dbescola/src/main/repository/DbescolaApplication.java
@@ -1,3 +1,13 @@
-public interface DbescolaApplication {
+package psii.senai.dbescola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbescolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbescolaApplication.class, args);
+	}
 
 }
