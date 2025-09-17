745025a550d6c8b431bfdfebba81afc6e7e7f3f0
vitorggoliveira-ops <vitor_gg_oliveira@estudante.sesisenai.org.br>
2025-09-17 16:07:35 -0300

    commit1

---
+++ dbescola/src/test/java/psii/dbescola/DbescolaApplicationTests.java
@@ -0,0 +1,13 @@
+package psii.dbescola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbescolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
